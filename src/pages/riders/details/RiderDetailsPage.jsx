fbd9f3aea178c389a4afb1b65b8d25fdac212181
Aaditya Sigdel <aadityasigdel953@gmail.com>
2025-07-21 18:07:48 +0545

    WorkingUPdateRider

--- src/pages/riders/details/RiderDetailsPage.jsx
+++ src/pages/riders/details/RiderDetailsPage.jsx
@@ -160,7 +160,7 @@ const RiderDetailsPage = () => {
 
 
 
-              {(rider.status === 'APPROVED' || rider.status === 'REJECTED') && (
+              {(rider.status === 'APPROVED' || rider.status === 'REJECTED' || rider.status === 'PENDING' ) && (
                 <Button
                   className="w-[120px] h-[39px] flex justify-center items-center bg-red-600 hover:bg-red-800"
                   onClick={() => setShowUpdateModal(true)}
